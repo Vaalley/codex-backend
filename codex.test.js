3b4c89774e89fc35af049d4bf900f42c5e1d11ca
Vaalley <vaaleyou@gmail.com>
2024-10-06 18:47:11 +0100

    updated tests to be more concise

--- codex.test.js
+++ codex.test.js
@@ -1,12 +1,6 @@
 // Test for checking if the endpoint get_platforms works and returns an array of platforms
 test("get_platforms", async () => {
-	const response = await fetch('http://127.0.0.1:3000/api/get-platforms', {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	})
-		.then(response => response.json());
+	const response = await codexFetch('get-platforms', { method: 'GET' })
 
 	expect(response).toBeInstanceOf(Array);
 	response.forEach(platform => {
@@ -19,14 +13,7 @@ test("get_platforms", async () => {
 
 // Test for checking if the endpoint get_platform_by_name works and returns a single platform object
 test("get_platform_by_name", async () => {
-	const response = await fetch('http://127.0.0.1:3000/api/get-platform-by-name', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({ name: 'Dreamcast' })
-	})
-		.then(response => response.json());
+	const response = await codexFetch('get-platform-by-name', { body: { name: 'Dreamcast' } })
 
 	expect(response).toBeInstanceOf(Object);
 	expect(response).toHaveProperty('name');
@@ -37,14 +24,9 @@ test("get_platform_by_name", async () => {
 
 // Test for checking if the endpoint get_platform_by_id works and returns a single platform object
 test("get_platform_by_id", async () => {
-	const response = await fetch('http://127.0.0.1:3000/api/get-platform-by-id', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({ id: '66eb0fae96ad1476e9e20c55' })
+	const response = await codexFetch('get-platform-by-id', {
+		body: { id: '66eb0fae96ad1476e9e20c55' }
 	})
-		.then(response => response.json());
 
 	expect(response).toBeInstanceOf(Object);
 	expect(response).toHaveProperty('name');
@@ -56,45 +38,42 @@ test("get_platform_by_id", async () => {
 // Test for checking if the endpoints add_platform, update_platform, and delete_platform work
 test('add, update, and delete platform', async () => {
 	// Add platform
-	const addResponse = await fetch('http://127.0.0.1:3000/api/add-platform', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
+	const addResponse = await codexFetch('add-platform', {
+		body: {
 			name: 'notaplatform',
 			manufacturer: 'notamanufacturer',
-		}),
-	});
-	const addedPlatform = await addResponse.json();
-	expect(addedPlatform).toHaveProperty('ID');
+		}
+	})
+	expect(addResponse).toHaveProperty('ID');
 
 	// Update platform
-	const updateResponse = await fetch('http://127.0.0.1:3000/api/update-platform', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			id: `${addedPlatform.ID}`,
+	const updateResponse = await codexFetch('update-platform', {
+		body: {
+			id: `${addResponse.ID}`,
 			name: 'updatedplatform',
 			manufacturer: 'updatedmanufacturer',
-		}),
-	});
-	const updatedPlatform = await updateResponse.json();
-	expect(updatedPlatform).toHaveProperty('ID');
-	expect(updatedPlatform.ID).toBe(addedPlatform.ID);
+		}
+	})
+	expect(updateResponse).toHaveProperty('ID');
+	expect(updateResponse.ID).toBe(addResponse.ID);
 
 	// Delete platform
-	const deleteResponse = await fetch('http://127.0.0.1:3000/api/delete-platform', {
+	const deleteResponse = await codexFetch('delete-platform', {
+		body: {
+			id: `${updateResponse.ID}`,
+		}
+	})
+	expect(deleteResponse.message).toBe('Platform deleted successfully');
+})
+
+function codexFetch(endpoint, params = {}) {
+	return fetch(`http://127.0.0.1:3000/api/${endpoint}`, {
 		method: 'POST',
+		...params,
+		body: JSON.stringify(params.body),
 		headers: {
 			'Content-Type': 'application/json',
+			...params.headers
 		},
-		body: JSON.stringify({
-			id: `${addedPlatform.ID}`,
-		}),
-	});
-	const deletedPlatform = await deleteResponse.json();
-	expect(deletedPlatform.message).toBe('Platform deleted successfully');
-})
+	}).then(response => response.json());
+}
